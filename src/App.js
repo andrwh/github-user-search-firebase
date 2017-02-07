cf8f0c5c3f7971f510eadecf9c86df213b41a928
Andrew Ho <andrew@kamcord.com>
2017-02-07 14:33:33 -0800

    hardcore firebase config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import firebase from 'firebase/app'
+import config from './config'
 
 // custom components
 import UserSearch from './components/UserSearch'
@@ -8,6 +10,18 @@ import 'ace-css/css/ace.min.css'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(config.firebaseConfig)
+  }
+
   render() {
     return (
       <div className="max-width-4 mx-auto">
---
+++ src/config.js
@@ -0,0 +1,12 @@
+// usually would place this in an environment config
+// for the sake of brevity, i'll hardcore this here
+
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyB8SxP82IIOfbBo-mpKo1ZONijoO9_9fhE",
+    authDomain: "reside-user-search.firebaseapp.com",
+    databaseURL: "https://reside-user-search.firebaseio.com",
+    storageBucket: "reside-user-search.appspot.com",
+    messagingSenderId: "148660920797"
+  }
+}
